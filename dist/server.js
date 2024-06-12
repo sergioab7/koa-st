ad3d7e1791c94d8a08f5e02308af6d386820e630
sergioab7 <sergioandujar7@gmail.com>
2024-06-12 22:26:00 +0200

    03-RUTA-REGISTRO-&&-REQUEST-BODY

--- dist/server.js
+++ dist/server.js
@@ -14,13 +14,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const koa_1 = __importDefault(require("koa"));
 const koa_router_1 = __importDefault(require("koa-router"));
+const koa_body_1 = __importDefault(require("koa-body"));
 const config_1 = __importDefault(require("./src/config"));
 const app = new koa_1.default();
+app.use((0, koa_body_1.default)({
+    multipart: true
+}));
 const PORT = config_1.default.PORT || 3002;
 const router = new koa_router_1.default();
 router.get("/", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    ctx.body = "Heyyyyyy";
+    ctx.body = "Server running ok";
 }));
+router.post("/register", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    const { usuario, email, password } = ctx.request.body;
+    ctx.body = {
+        usuario,
+        email,
+        password
+    };
+}));
+// Dont touch
 app
     .use(router.routes())
     .use(router.allowedMethods());
--- server.ts
+++ server.ts
@@ -1,17 +1,34 @@
 import Koa from "koa"
 import Router from "koa-router"
+import bodyParser from "koa-body"
 import config from "./src/config";
 
 const app = new Koa();
+
+app.use(bodyParser({
+    multipart:true
+}))
+
 const PORT = config.PORT || 3002
 const router = new Router();
 
 
 router.get("/", async ctx => {
-    ctx.body = "Heyyyyyy"
+    ctx.body = "Server running ok"
+})
+
+router.post("/register", async ctx => {
+    const {usuario,email,password} = ctx.request.body
+    ctx.body = {
+        usuario,
+        email,
+        password
+    }
 })
 
 
+
+// Dont touch
 app
     .use(router.routes())
     .use(router.allowedMethods());
